ea13af794f859e298ac2f9ae5aa6be4d8c0c7415
Priyanshu Pareek <120202241+Pareek3377@users.noreply.github.com>
2025-07-07 18:13:46 +0000

    strings

--- datatypes.-summary.js
+++ datatypes.-summary.js
@@ -1,4 +1,4 @@
-// Primitive 
+ // Primitive 
 
 // 7 types or categories: String , Number, Boolean, Null, Undefined, Symbol,BigInt 
 
@@ -12,9 +12,9 @@ let userEmail;
 const id = Symbol('123')
 const anotherId= Symbol('123')
 
-/* console.log(id==anotherId); */
+ console.log(id==anotherId); 
 
-/* const bigNumber = 3472598739 */
+ const bigNumber = 3472598739 
 
 // Reference (Non- Primitive)
 
@@ -32,3 +32,29 @@ const myFunction=function(){
 }
 console.log(typeof bigNumber);
 console.log(typeof heroes);
+ 
+ https://262.ecma-international.org/15.0/index.html?_gl=1*1lbzmrg*_ga*NzUzNzAwOTY5LjE3NTE4NzAxMjA.*_ga_TDCK4DWEPP*czE3NTE4NzAxMjAkbzEkZzAkdDE3NTE4NzAxMjAkajYwJGwwJGgw */
+
+//*******************MEMORIES:******************/
+
+// Stack(Primitive), Heap(Non-Primitive)*/
+
+let myYoutubename = "PriyanshuPareekvlogs"
+
+let anothername = myYoutubename
+anothername="seribal assasin"
+
+console.log(myYoutubename);
+console.log(anothername);
+
+let user1={
+    email: "user@google.com",
+    upi : "user@ybl"
+}
+let user2= user1
+
+user2.email= "priyanshu@google.com"
+
+console.log(user1.email);
+console.log(user2.email);
+
