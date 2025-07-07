00e059c23bbc95e790d64a0f03238927101bc4d4
Priyanshu Pareek <120202241+Pareek3377@users.noreply.github.com>
2025-07-07 06:41:46 +0000

    Datatype-summary

---
+++ datatypes.-summary.js
@@ -0,0 +1,34 @@
+// Primitive 
+
+// 7 types or categories: String , Number, Boolean, Null, Undefined, Symbol,BigInt 
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null 
+let userEmail;
+
+const id = Symbol('123')
+const anotherId= Symbol('123')
+
+/* console.log(id==anotherId); */
+
+/* const bigNumber = 3472598739 */
+
+// Reference (Non- Primitive)
+
+//Array, Objects, Functions, 
+
+const heroes= ["shaktimaan","Hero","Doga"];
+let myObj={
+    name: "priyanshu",
+    age: 22,
+
+}
+const myFunction=function(){
+    console.log("Hello World");
+    
+}
+console.log(typeof bigNumber);
+console.log(typeof heroes);
