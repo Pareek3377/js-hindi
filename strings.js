2006da763e743d0973b50f50b4b1a283dbc6bee7
Priyanshu Pareek <120202241+Pareek3377@users.noreply.github.com>
2025-07-08 06:48:27 +0000

    The backtick (`) is a typographical mark primarily used in computing and is also known as backquote or grave accent

---
+++ strings.js
@@ -0,0 +1,11 @@
+const name="Priyanshu"
+const repoCount = 13
+
+//console.log(name + repoCount +"Value");  //wrong syntax nowadays
+
+console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('Priyanshupp')
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
