cb1276583977ecc1d0a6f26044dd966b3aeba839
Priyanshu Pareek <120202241+Pareek3377@users.noreply.github.com>
2025-07-09 14:13:38 +0000

    Arrays

--- arrays.js
+++ arrays.js
@@ -1,31 +1,30 @@
-///Arrays
+//Arrays
 
-//const myArr= [0,1,2,3,4,5]
-//const myHeroes=["thor","ironman","captain america"];
+const myArr= [0,1,2,3,4,5]
+const myHeroes=["thor","ironman","captain america"];
 
-/*console.log(myArr[0]);
+console.log(myArr[0]);
 console.log(myHeroes);
 
 //Array methods
 
-/* myArr.push(6)
+myArr.push(6)
 myArr.push(8)
 console.log(myArr);
 myArr.pop();
 
 console.log(myArr);
-myArr.unshift(9) */
-/* myArr.shift()
+myArr.unshift(9) 
+ myArr.shift()
 console.log(myArr);
 console.log(myArr.indexOf(9));
 console.log(myArr.indexOf(4));
-console.log(myArr.includes(9)); */
-//const newArr = myArr.join()
+console.log(myArr.includes(9)); 
+const newArr = myArr.join()
 
-//console.log(myArr);
-//console.log( newArr); 
-
-/* console.log("A", myArr);
+console.log(myArr);
+console.log( newArr); 
+console.log("A", myArr);
 
 const myn1=myArr.slice(1,3)
 console.log(myn1);
@@ -33,26 +32,26 @@ console.log("B",myArr);
 
 const myn2=myArr.slice(1,3)
 console.log(myn2);
- */
+ 
 
 const marvel_Heroes=["thor","ironman","captain america"];
 const dc_Heroes=["superman","Batman","Flash"];
 
-//marvel_Heroes.push(dc_Heroes)
+marvel_Heroes.push(dc_Heroes)
 
-//console.log(marvel_Heroes);
-//console.log(marvel_Heroes[3][1]);
-//const allHeroes= marvel_Heroes.concat(dc_Heroes)
-//console.log(allHeroes);
+console.log(marvel_Heroes);
+console.log(marvel_Heroes[3][1]);
+const allHeroes= marvel_Heroes.concat(dc_Heroes)
+console.log(allHeroes);
 
 const all_new_heroes=[...marvel_Heroes,...dc_Heroes];//spread operator
 
-//console.log(all_new_heroes);
+console.log(all_new_heroes);
 
-/* const another_array=[1,2,3,[4,5,6],7,[8,7,[4,5]]]
+ const another_array=[1,2,3,[4,5,6],7,[8,7,[4,5]]]
 
 const real_another_array= another_array.flat(Infinity)
-console.log(real_another_array); */
+console.log(real_another_array); 
 
 console.log(Array.isArray("Priyanshu"));
 console.log(Array.from("Priyanshu"));
